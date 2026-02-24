afca2335af559cd56de38ced7ff68b525257cc9e
camilo9508 <camilo.luna145@gmail.com>
2026-02-24 09:07:40 -0500

    subo archivo desde pc cabecera

---
+++ funciones/ClasesObjetos/herencia.py
@@ -0,0 +1,9 @@
+class Animal:
+    def comer(self):
+        print("Como muchas veces al dia")
+    def dormir(self):
+      print("duermo muchas horas al dia")
+      
+class Perro(Animal):
+    def hacer_sonido(self):
+        print("guau guau")
